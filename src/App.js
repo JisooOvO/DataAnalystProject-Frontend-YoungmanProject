c598f502b6d3ab749145edf8f517c05281b99443
JisooOvO <fdking1887@naver.com>
2023-12-21 15:26:58 +0900

    add font

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import WhiteBox from "./component/common/WhiteBox";
 import "./index.css"
 
 function App() {
   return (
-    <div className="App flex gap-10 w-full h-full">
-      <div className="text-xl w-[20%] h-[40%]">
-        <WhiteBox title={"박민호"}/>
-      </div>
-      <div className="text-2xl font-bold w-[50%] h-[20%]">
-        <WhiteBox title={"호호호"}/>
-      </div>
+    <div className="App">
+
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode : "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-blue" : "#57677E"
+      }
+    },
   },
   plugins: [],
 }
