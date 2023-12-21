0ed36bcc4a170ab18005d879e1b6f71994214583
JisooOvO <fdking1887@naver.com>
2023-12-21 15:13:23 +0900

    fixed conflict

---
+++ src/component/common/WhiteBox.js
@@ -0,0 +1,10 @@
+const WhiteBox = ({title}) => {
+    return (
+      <div className="border border-black bg-white shadow-md rounded-lg text-inherit p-5 h-full w-full">
+          {title}
+      </div>
+    )
+  }
+  
+  export default WhiteBox
+  
