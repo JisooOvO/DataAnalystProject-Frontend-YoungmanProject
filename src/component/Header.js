4ac5047272f191cf969ff6723a4445c57e924f13
777Mino777 <petctusweet@naver.com>
2023-12-22 10:45:58 +0900

    updated

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,7 +6,7 @@ import RegisterIcon from "../image/RegisterIcon"
 import WriteIcon from "../image/WriteIcon"
 
 const Header = () => {
-
+    
     const menuItems = [
         {
             title: "사입 관리",
@@ -24,15 +24,20 @@ const Header = () => {
         }
     ]
 
-    const isLoggedIn = () => {
-        localStorage.getItem("token" && "username" != null);
-    }
-
     const navigate = useNavigate();
+
+    const isLoggedIn = sessionStorage.getItem("token" != null);
 
     const handleLoginButton = (e) => {
         e.preventDefault();
         navigate("/login")
+    }
+
+    const handleLogoutButton = (e) => {
+        e.preventDefault();
+        sessionStorage.removeItem("token");
+        sessionStorage.removeItem("username");
+        navigate("/login");
     }
 
     return (
@@ -40,14 +45,23 @@ const Header = () => {
             <div className="w-full h-[12%] border-b-[1px] border-black">
                 <h1 className="pt-3 pl-4 h-[50%] w-full text-white text-lg font-bold">수기 영수증 변환 시스템</h1>
                 <div className="h-[50%] w-full flex">
-                    <button onClick={handleLoginButton} className="w-[50%] flex text-xl pt-2 pl-5">
+                    { isLoggedIn ?
+                      (  <button onClick={handleLoginButton} className="w-[50%] flex text-xl pt-2 pl-5">
                         <PowerIcon />
                         <div className=" text-white text-[19px] pl-1 pt-[1px]">
-                            { isLoggedIn ? "로그아웃" : "로그인"}
+                            로그인
                             </div>
                     </button>
+                      ) : (
+                    <button onClick={handleLogoutButton} className="w-[50%] flex text-xl pt-2 pl-5">
+                        <PowerIcon />
+                        <div className=" text-white text-[19px] pl-1 pt-[1px]">
+                            로그아웃
+                            </div>
+                </button>  )
+                    }
                     <div className="w-[50%] text-[#bdb3b3] text-[15px] pt-3" >
-                         { isLoggedIn ? `${sessionStorage.username}님 환영합니다` : "로그인을 해주세요" }
+                         { isLoggedIn ?   '로그인 하세요' : `${sessionStorage.getItem("username")}님 환영합니다`} 
                         </div>
                 </div>
             </div>
