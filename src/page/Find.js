cab1513ee496ebb1d0574c2ec0d1bbef0524b0cc
777Mino777 <petctusweet@naver.com>
2023-12-22 17:56:36 +0900

    add find page

---
+++ src/page/Find.js
@@ -0,0 +1,11 @@
+import TitleHeader from "../component/common/TitleHeader"
+
+const FindId = () => {
+  return (
+    <div className="w-full max-w-[30rem] h-full mx-auto">
+      <TitleHeader title />
+    </div>
+  )
+}
+
+export default FindId
